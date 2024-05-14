99f15474591e5f9b45dfb81ac2757f6cce52105c
Ruben Bensimon <rubensbensimon@Gmail.com>
2024-05-14 20:39:37 +0300

    coucou raph

--- mailer.php
+++ mailer.php
@@ -1,53 +1,65 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+//Load Composer's autoloader
 require 'vendor/autoload.php';
 
-error_reporting(E_ALL); // Afficher toutes les erreurs
-ini_set('display_errors', 1);
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'envoyeurdemail@gmail.com';                     //SMTP username
+    $mail->Password   = 'tkas eqsc krnu wnrb';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    try {
-        $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'envoyeurdemails@gmail.com';
-        $mail->Password   = 'tkas eqsc krnu wnrb'; // //mbst hnsm fprf uhxd
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->Port       = 465;
+    //Recipients
+    $mail->setFrom('envoyeurdemail@gmail.com', 'Mailer');
+    $mail->addAddress('rubensbensimon@gmail.com', 'Joe User');     //Add a recipient
+   // $mail->addAddress('ellen@example.com');               //Name is optional
 
-        $mail->setFrom('invitecard.website@gmail.com', 'Mailer');
-        $mail->addAddress('rubensbensimon@gmail.com', 'Joe User');
+    //Attachments
+   
 
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prénom'];
-        $vientPas = isset($_POST['vientPas']) ? 'Oui' : 'Non';
-        $mairie = isset($_POST['mairie']) ? 'Oui' : 'Non';
-        $houpa = isset($_POST['houpa']) ? 'Oui' : 'Non';
-        $reception = isset($_POST['reception']) ? 'Oui' : 'Non';
-        $numberOfGuests = $_POST['numberOfGuests'];
-        $messageMariés = $_POST['messageMariés'];
 
-        $emailbody = "Nom: $nom\n";
-        $emailbody .= "Prénom: $prenom\n";
-        $emailbody .= "Vient pas: $vientPas\n";
-        $emailbody .= "Mairie: $mairie\n";
-        $emailbody .= "Houpa: $houpa\n";
-        $emailbody .= "Reception: $reception\n";
-        $emailbody .= "Nombre d'invités: $numberOfGuests\n";
-        $emailbody .= "Message aux mariés:\n$messageMariés\n";
+    $nom = $_POST ['nom'];
+    $prenom = $_POST ['prenom'];
+    $assisstepas = isset($_POST ['vientpas']) ? "oui" : "non";
+    $assisstemairie = isset($_POST ['mairie']) ? "oui" : "non";
+    $assisstehoupa = isset($_POST ['houpa']) ? "oui" : "non";
+    $assisstesoiree = isset($_POST['reception']) ? "oui" : "non";
+    $numofguest = $_POST['numberOfGuests'];
+    $messagepourlesmariés = $_POST['messageMariés'];
 
-        $mail->Subject = 'RSVP Form Submission';
-        $mail->Body    = $emailbody;
+    $mailbody .= "Nom : $nom\n";
+    $mailbody.= "Prénom : $prenom\n";
+    $mailbody.= "N'Assistera pas à la cérémonie : $assisstepas\n";
+    $mailbody.= "Assistera à la cérémonie à la mairie : $assisstemairie\n";
+    $mailbody.= "Assistera à la cérémonie à la houpa : $assisstehoupa\n";
+    $mailbody.= "Assistera à la soirée de réception : $assisstesoiree\n";
+    $mailbody.= "Nombre d'invités : $numofguest\n";
+    $mailbody.= "Message pour les mariés :\n $messagepourlesmariés\n";
 
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = $mailbody;
+    $mail->AltBody = $mailbody;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-?>
+
+
